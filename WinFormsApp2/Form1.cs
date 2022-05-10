759d96fdcb325afa1bbb6af22b09874816d0e3ba
MarlinShenoda <marlinadel2000@gmail.com>
2022-05-10 17:05:03 +0200

    Add project files.

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string namn = textBox1.Text;
+            string lön = textBox2.Text;
+
+            MessageBox.Show(namn + "ha lön:" + lön);
+        }
+    }
+}
